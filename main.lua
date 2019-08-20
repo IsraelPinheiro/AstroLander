b5794bd5f249c5ab738c4a4bf6ed5707913cd3c8
israel reis pinheiro <1818210>
2019-08-20 18:11:33 -0300

    Add auxiliar variables
    Former-commit-id: ac88c5c4a28eccb46e0ade1aa42c9b2206606338

--- main.lua
+++ main.lua
@@ -4,12 +4,15 @@ display.setStatusBar(display.HiddenStatusBar)
 centerX = display.contentCenterX
 centerY = display.contentCenterY
 
+planets = {"assets/img/planets/moon.png", "assets/img/planets/mars.png", "assets/img/planets/neptune.png"}
+ships_full = {"assets/img/ships/apollo_full.png", "assets/img/ships/dragon_full.png", "assets/img/ships/soyuz_full.png"}
+ships_mini = {"assets/img/ships/apollo_mini.png", "assets/img/ships/dragon_mini.png", "assets/img/ships/soyuz_mini.png"}
+
 screenWidth = display.contentWidth
 screenHeight = display.contentHeight
 
 selectedPlanet = nil
 selectedShip = nil
 bestScore = 0
-
 
 composer.gotoScene("scenes.start", { params={} })
