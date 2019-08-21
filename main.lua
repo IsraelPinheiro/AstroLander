f02abafb3f3240ce6a778601d82a69af7137d825
israel reis pinheiro <1818210>
2019-08-20 21:44:11 -0300

    Refactor to use ImageSheets
    Former-commit-id: ade2152603e9f8f861018426f79b9e9dddd345a1

--- main.lua
+++ main.lua
@@ -4,15 +4,37 @@ display.setStatusBar(display.HiddenStatusBar)
 centerX = display.contentCenterX
 centerY = display.contentCenterY
 
-planets = {"assets/img/planets/moon.png", "assets/img/planets/mars.png", "assets/img/planets/neptune.png"}
-ships_full = {"assets/img/ships/apollo_full.png", "assets/img/ships/dragon_full.png", "assets/img/ships/soyuz_full.png"}
-ships_mini = {"assets/img/ships/apollo_mini.png", "assets/img/ships/dragon_mini.png", "assets/img/ships/soyuz_mini.png"}
-
 screenWidth = display.contentWidth
 screenHeight = display.contentHeight
 
+-- Control Variables
 selectedPlanet = 1
 selectedShip = 1
 bestScore = 0
+
+-- Planets
+planets = graphics.newImageSheet("assets/img/planets.png", {
+    width = 500,
+    height = 500,
+    numFrames = 3
+})
+
+planets_name = { "Moon","Mars","Neptune" }
+planets_G = { 1.62, 3.71, 11.15 }
+
+-- Ships
+ships_full = graphics.newImageSheet("assets/img/ships_full.png", {
+    width = 500,
+    height = 500,
+    numFrames = 3
+})
+
+ships_mini = graphics.newImageSheet("assets/img/ships_mini.png", {
+    width = 80,
+    height = 80,
+    numFrames = 3
+})
+
+ships_name = { "Dragon","Apollo Lander","Soyuz" }
 
 composer.gotoScene("scenes.start", { params={} })
