9dda309dd6a0288a147f411a53135f192c85947d
Israel R Pinheiro <israel.pinheiro@live.com>
2019-08-21 00:23:27 -0300

    Detail new Image Sheets and organize code
    Former-commit-id: ce57a6f7eddf4fb56068b2044764c485244673bf

--- main.lua
+++ main.lua
@@ -12,11 +12,22 @@ selectedPlanet = 1
 selectedShip = 1
 bestScore = 0
 
+-- Logos
+logos = graphics.newImageSheet("assets/img/logos.png", {
+    width = 2160,
+    height = 500,
+    numFrames = 3,
+    sheetContentWidth = 2160,
+    sheetContentHeight = 1500
+})
+
 -- Planets
 planets = graphics.newImageSheet("assets/img/planets.png", {
     width = 500,
     height = 500,
-    numFrames = 3
+    numFrames = 3,
+    sheetContentWidth = 1500,
+    sheetContentHeight = 500
 })
 
 planets_name = { "Moon","Mars","Neptune" }
@@ -24,17 +35,54 @@ planets_G = { 1.62, 3.71, 11.15 }
 
 -- Ships
 ships_full = graphics.newImageSheet("assets/img/ships_full.png", {
-    width = 500,
-    height = 500,
-    numFrames = 3
+    frames ={
+        {   -- Apollo Lander
+            x = 0,
+            y = 0,
+            width = 810,
+            height = 500
+        },
+        {   -- Dragon
+            x = 810,
+            y = 0,
+            width = 434,
+            height = 500
+        }
+        ,
+        {   -- Soyuz
+            x = 1244,
+            y = 0,
+            width = 202,
+            height = 500
+        }
+    }
 })
 
 ships_mini = graphics.newImageSheet("assets/img/ships_mini.png", {
-    width = 80,
-    height = 80,
-    numFrames = 3
+    frames ={
+        {   -- Apollo Lander
+            x = 0,
+            y = 0,
+            width = 138,
+            height = 85
+        },
+        {   -- Dragon
+            x = 810,
+            y = 0,
+            width = 74,
+            height = 85
+        }
+        ,
+        {   -- Soyuz
+            x = 1244,
+            y = 0,
+            width = 35,
+            height = 85
+        }
+    }
 })
 
 ships_name = { "Dragon","Apollo Lander","Soyuz" }
 
+-- Go To Star Scene
 composer.gotoScene("scenes.start", { params={} })
