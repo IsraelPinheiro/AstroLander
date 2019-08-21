a7e95b894e3dbf6e4055a65ffe5df6b81594a4a7
Israel R Pinheiro <israel.pinheiro@live.com>
2019-08-21 13:00:03 -0300

    Add initial UI
    Former-commit-id: db60d82f7582c85977302aee9c2daa6a3d5f7184

--- scenes/ships.lua
+++ scenes/ships.lua
@@ -1,7 +1,35 @@
 local composer = require( "composer" )
  
 local scene = composer.newScene()
- 
+
+
+local function nextShip(event)
+    if ( event.phase == "ended" ) then
+        if (selectedShip == 3) then
+            selectedShip = 1
+        else
+            selectedShip = selectedShip + 1
+        end
+        Ship:setFrame(selectedShip)
+    end
+end
+
+local function previousShip(event)
+    if ( event.phase == "ended" ) then
+        if (selectedShip == 1) then
+            selectedShip = 3
+        else
+            selectedShip = selectedShip - 1
+        end
+        Ship:setFrame(selectedShip)
+    end
+end
+
+local function launchShip(event)
+    if ( event.phase == "ended" ) then
+        composer.gotoScene("scenes.game", { params={} })
+    end
+end
 -- -----------------------------------------------------------------------------------
 -- Code outside of the scene event functions below will only be executed ONCE unless
 -- the scene is removed entirely (not recycled) via "composer.removeScene()"
@@ -19,22 +47,51 @@ function scene:create( event )
  
     local sceneGroup = self.view
     -- Code here runs when the scene is first created but has not yet appeared on screen
- 
+
+
+    -- Background Image
+    local background = display.newImage("assets/img/ui/background.png")
+    background.x, background.y = centerX, centerY
+    background.width, background.height = screenWidth*2, screenHeight*2
+
+
+    -- Planet
+    ship = display.newSprite(ships, {start=1, count=3 })
+    ship.anchorX, ship.anchorY = 0.5, 0.5
+    ship.x, ship.y = centerX, ship.height/2 + 130
+
+    -- Buttons
+    -- Arrow Left
+    local buttonLeft = display.newImage("assets/img/ui/left.png")
+    buttonLeft.anchorX, buttonLeft.anchorY = 0, 0.5 
+    buttonLeft.x, buttonLeft.y = 50, centerY
+    buttonLeft:addEventListener( "touch", previousShip)
+
+    -- Arrow Right
+    local buttonRight = display.newImage("assets/img/ui/right.png")
+    buttonRight.anchorX, buttonRight.anchorY = 1, 0.5
+    buttonRight.x, buttonRight.y = screenWidth-50, centerY
+    buttonRight:addEventListener( "touch", nextShip)
+
+    -- Select
+    local buttonSelect = display.newImage("assets/img/ui/select.png")
+    buttonSelect.anchorX, buttonSelect.anchorY = 0.5, 1
+    buttonSelect.x, buttonSelect.y = centerX, screenHeight -50
+
+    buttonSelect:addEventListener( "touch", launchShip)
+    
 end
  
  
 -- show()
 function scene:show( event )
- 
     local sceneGroup = self.view
     local phase = event.phase
  
     if ( phase == "will" ) then
         -- Code here runs when the scene is still off screen (but is about to come on screen)
- 
     elseif ( phase == "did" ) then
         -- Code here runs when the scene is entirely on screen
- 
     end
 end
  
@@ -47,10 +104,8 @@ function scene:hide( event )
  
     if ( phase == "will" ) then
         -- Code here runs when the scene is on screen (but is about to go off screen)
- 
     elseif ( phase == "did" ) then
         -- Code here runs immediately after the scene goes entirely off screen
- 
     end
 end
  
