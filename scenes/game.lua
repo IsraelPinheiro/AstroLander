09ff096f2808d68396e034c28c8c2a6d231b29d9
Israel R Pinheiro <israel.pinheiro@live.com>
2019-08-28 17:22:31 -0300

    Initial UI Elements

--- scenes/game.lua
+++ scenes/game.lua
@@ -1,69 +1,91 @@
 local composer = require( "composer" )
 local scene = composer.newScene()
 composer.recycleOnSceneChange = true
- 
--- -----------------------------------------------------------------------------------
--- Code outside of the scene event functions below will only be executed ONCE unless
--- the scene is removed entirely (not recycled) via "composer.removeScene()"
--- -----------------------------------------------------------------------------------
- 
- 
- 
- 
+
+-- SFX
+local gameMusic
+local sfx_thrust
+local sfx_pause
+
+-- UI Elements
+local background
+
+local fuelBar
+local fuelBarFill
+
+local buttonPause
+local buttonLeft
+local buttonRight
+local buttonThrust
+
+-- Event Functions
+local function pause(event)
+    if ( event.phase == "ended" ) then
+       
+    end
+end
+
+
+
 -- -----------------------------------------------------------------------------------
 -- Scene event functions
 -- -----------------------------------------------------------------------------------
- 
+
 -- create()
-function scene:create( event )
- 
+function scene:create( event ) 
     local sceneGroup = self.view
-    -- Code here runs when the scene is first created but has not yet appeared on screen
- 
+
+    -- Load Music
+    bgMusic = audio.loadSound("assets/sounds/music/Lockdown.mp3")
+
+    -- Load SFX
+    -- TODO: Load SFX
+
+    -- Background Image
+    background = display.newImage("assets/img/ui/background.png")
+    background.x, background.y = centerX, centerY
+    background.width, background.height = screenWidth*1.5, screenHeight*1.5
+
+    --FuelBar
+
+    -- Pause Button
+    buttonPause = display.newImage("assets/img/ui/pause.png")
+    buttonPause.anchorX, buttonPause.anchorY = 0, 0
+    buttonPause.x, buttonPause.y = 30, 30
+    buttonPause:addEventListener( "touch", pause)
+
 end
- 
- 
+
 -- show()
 function scene:show( event )
- 
     local sceneGroup = self.view
     local phase = event.phase
- 
+
     if ( phase == "will" ) then
-        -- Code here runs when the scene is still off screen (but is about to come on screen)
- 
+
     elseif ( phase == "did" ) then
-        -- Code here runs when the scene is entirely on screen
- 
+
     end
 end
- 
- 
+
 -- hide()
 function scene:hide( event )
- 
     local sceneGroup = self.view
     local phase = event.phase
- 
+
     if ( phase == "will" ) then
-        -- Code here runs when the scene is on screen (but is about to go off screen)
- 
+
     elseif ( phase == "did" ) then
-        -- Code here runs immediately after the scene goes entirely off screen
- 
+
     end
 end
- 
- 
+
 -- destroy()
 function scene:destroy( event )
- 
     local sceneGroup = self.view
-    -- Code here runs prior to the removal of scene's view
- 
+
 end
- 
- 
+
 -- -----------------------------------------------------------------------------------
 -- Scene event function listeners
 -- -----------------------------------------------------------------------------------
