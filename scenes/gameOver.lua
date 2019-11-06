cd7deea8c84ad5c585f6ecb75974cf706e55ff55
Israel R Pinheiro <israel.pinheiro@live.com>
2019-11-06 00:35:54 -0300

    add basic events and controlls

--- scenes/gameOver.lua
+++ scenes/gameOver.lua
@@ -6,6 +6,69 @@ composer.recycleOnSceneChange = true
 -- Activate Multitouch
 system.activate( "multitouch" )
 
+
+-- Load Music
+local bgMusic = audio.loadSound("assets/sounds/music/Lockdown.mp3")
+
+-- UI Elements
+local UIDefaultFont = "assets/fonts/ConsoleClassic.ttf"
+local background
+local buttonTryAgain
+local buttonBackStart
+
+
+-- Event Functions
+local function tryAgain(event)
+    if ( event.phase == "began" ) then
+        
+        
+    end
+    if ( event.phase == "ended" ) then
+        
+        
+    end
+end
+local function backStart(event)
+    if ( event.phase == "began" ) then
+        
+        
+    end
+    if ( event.phase == "ended" ) then
+        
+        
+    end
+end
+
+-- Keyboard controls handler
+local function onKeyEvent( event )
+    if (system.getInfo( "platform" ) == "win32" or system.getInfo( "platform" ) == "macos" ) then
+
+    end
+
+    if(event.keyName == "space" or event.keyName == "buttonA" or event.keyName == "buttonStart") then
+        if ( event.phase == "down" ) then
+            audio.play(sfx_back)
+            composer.gotoScene("scenes.game")
+        end
+    end
+
+    if(event.keyName == "escape" or event.keyName == "buttonB" or event.keyName == "buttonSelect") then
+        if ( event.phase == "down" ) then
+            audio.play(sfx_back)
+            composer.gotoScene("scenes.start")
+        end
+    end
+ 
+    -- If the "back" key was pressed on Android, prevent it from backing out of the app
+    if (event.keyName == "back") then
+        if (system.getInfo("platform") == "android" ) then
+            return true
+        end
+    end
+
+    return false
+end
+
 -- -----------------------------------------------------------------------------------
 -- Scene event functions
 -- -----------------------------------------------------------------------------------
@@ -13,7 +76,38 @@ system.activate( "multitouch" )
 -- create()
 function scene:create( event )
     local sceneGroup = self.view
-    -- Code here runs when the scene is first created but has not yet appeared on screen
+    
+    -- Background Image
+    background = display.newImage("assets/img/ui/background.png")
+    background.x, background.y = centerX, centerY
+    background.width, background.height = screenWidth*1.5, screenHeight*1.5
+
+    -- Text
+    if(lastScore>0) then
+        GameOverText = display.newText("You Landed!", centerX, centerY, UIDefaultFont, 150 )
+    else
+        GameOverText = display.newText("Game Over", centerX, centerY, UIDefaultFont, 150 )
+    end
+    GameOverText.anchorX, GameOverText.anchorY = 0.5,0.5
+
+    -- Game Score
+    GameScore = display.newText(lastScore.." Points", centerX, centerY+100, UIDefaultFont, 150 )
+    GameScore.anchorX, GameScore.anchorY = 0.5,0
+
+    -- Try Again Button
+    buttonTryAgain = display.newImage("assets/img/ui/tryAgain.png")
+    buttonTryAgain.anchorX, buttonTryAgain.anchorY = 0, 1
+    buttonTryAgain.x, buttonTryAgain.y = 0, screenHeight
+    buttonTryAgain:addEventListener("touch", tryAgain)
+
+    -- Back to Start Button
+    buttonBackStart = display.newImage("assets/img/ui/backStart.png")
+    buttonBackStart.anchorX, buttonBackStart.anchorY = 0, 1
+    buttonBackStart.x, buttonBackStart.y = screenWidth/2, screenHeight
+    buttonBackStart:addEventListener("touch", backStart)
+
+    -- Keyboard Listener
+    Runtime:addEventListener("key", onKeyEvent)
 end
  
  
@@ -36,7 +130,11 @@ function scene:hide( event )
     local phase = event.phase
 
     if ( phase == "will" ) then
-        -- Code here runs when the scene is on screen (but is about to go off screen)
+        audio.stop()
+        -- Remove Event Listeners
+        buttonTryAgain:removeEventListener("touch", tryAgain)
+        buttonBackStart:removeEventListener("touch", backStart)
+        Runtime:removeEventListener("key", onKeyEvent)
 
     elseif ( phase == "did" ) then
         -- Code here runs immediately after the scene goes entirely off screen
